cce988b8f2ef744fa704a7dfbe6c1644843ee113
Wenyuan Huang <157926136+whuang369@users.noreply.github.com>
2025-10-22 20:15:17 -0500

    update training scripts

--- examples/ppo_trainer/run_ppo.sh
+++ examples/ppo_trainer/run_ppo.sh
@@ -1,18 +1,18 @@
 python3 -m verl.trainer.main_ppo \
     data.train_files=$DATA_DIR/train.parquet \
     data.val_files=$DATA_DIR/test.parquet \
-    data.train_batch_size=512 \
-    data.val_batch_size=128 \
+    data.train_batch_size=128 \
+    data.val_batch_size=32 \
     data.max_prompt_length=1024 \
     data.max_response_length=512 \
     actor_rollout_ref.model.path=$BASE_MODEL \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=128 \
-    actor_rollout_ref.actor.ppo_micro_batch_size=8 \
-    actor_rollout_ref.actor.fsdp_config.param_offload=False \
-    actor_rollout_ref.actor.fsdp_config.grad_offload=False \
-    actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
+    actor_rollout_ref.actor.ppo_mini_batch_size=32 \
+    actor_rollout_ref.actor.ppo_micro_batch_size=2 \
+    actor_rollout_ref.actor.fsdp_config.param_offload=True \
+    actor_rollout_ref.actor.fsdp_config.grad_offload=True \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
     actor_rollout_ref.rollout.tensor_model_parallel_size=$ROLLOUT_TP_SIZE \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.3 \
@@ -20,7 +20,7 @@ python3 -m verl.trainer.main_ppo \
     critic.optim.lr=1e-5 \
     critic.model.use_remove_padding=True \
     critic.model.path=$BASE_MODEL \
-    critic.model.enable_gradient_checkpointing=False \
+    critic.model.enable_gradient_checkpointing=True \
     critic.ppo_mini_batch_size=128 \
     critic.ppo_micro_batch_size=8 \
     critic.model.fsdp_config.param_offload=True \
